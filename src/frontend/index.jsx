c314492ecc67b7ac9ef209304e44251e62b59a11
JibiteshI528974 <jibitesh.chakraborty@sap.com>
2025-02-04 10:27:05 +0530

    New commit

--- src/frontend/index.jsx
+++ src/frontend/index.jsx
@@ -1,150 +1,230 @@
 import React, { useEffect, useState } from 'react';
-import ForgeReconciler, { Button, Text, Label, Textfield, useProductContext, Select, DynamicTable } from '@forge/react';
+import ForgeReconciler, {
+  Button,
+  Text,
+  Label,
+  Textfield,
+  useProductContext,
+  Select,
+  DynamicTable,
+  Modal,
+  ModalTransition,
+  ModalHeader,
+  ModalTitle,
+  ModalBody,
+  ModalFooter,
+} from '@forge/react';
 import { invoke } from '@forge/bridge';
 
 const App = () => {
-  const [data, setData] = useState(null);
   const [currentPage, setCurrentPage] = useState('list');
-  const [projectVersions, setProjectVersions] = useState(null); 
-  const [loading, setLoading] = useState(false); 
-  const [error, setError] = useState(null); 
-  const [selectedVersion, setSelectedVersion] = useState(''); 
-  const [name, setname] = useState('');
-  const [inumber, setinumber] = useState('');
-  const [records, setrecords] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [selectedVersion, setSelectedVersion] = useState('');
+  const [deliveryName, setDeliveryName] = useState('');
+  const [projectVersions, setProjectVersions] = useState([]);
+  const [deliveries, setDeliveries] = useState([]);
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [apiResponse, setApiResponse] = useState([]);
+  const [isAddDeliveryModalOpen, setIsAddDeliveryModalOpen] = useState(false);
 
   const context = useProductContext();
+  const projectKey = context?.extension?.project?.key;
+  const projectId = context?.extension?.project?.id; 
 
-  console.log('Context:', context); 
-
-  const goToList = async () => {
-    console.log(selectedVersion);
-    console.log(name);
-    console.log(inumber);
-    await invoke('addrecords', { selectedVersion, name, inumber });
-    setCurrentPage('list'); 
-  };
-
-  const goToEntry = () => {
-    setCurrentPage('entry');
-  };
+  console.log('Context:', context);
 
   
   useEffect(() => {
-    if (context) {
-      const projectKey = context.extension.project.key; 
+    if (projectKey) {
       fetchProjectVersions(projectKey);
     }
-  }, [context]);
+  }, [projectKey]);
 
   
+  useEffect(() => {
+    if (currentPage === 'list') {
+      invoke('getDeliveriesFromStorage').then(setDeliveries);
+    }
+  }, [currentPage]);
+
   const fetchProjectVersions = async (projectKey) => {
     setLoading(true);
     setError(null);
 
     try {
-      const versions = await invoke('getProjectVersions', { projectKey }); 
+      const versions = await invoke('getProjectVersions', { projectKey });
       setProjectVersions(versions);
     } catch (error) {
       console.error('Error fetching project versions:', error);
-      setError(error.message);
+      setError('Failed to fetch project versions.');
     } finally {
       setLoading(false);
     }
   };
 
-  useEffect(() => {
-    console.log('Hi');
-    console.log(context); 
-    invoke('getText', { example: 'my-invoke-variable' }).then(setData);
-  }, [context]); 
+  const handleSubmit = async () => {
+    setLoading(true);
+    setError(null);
 
-  const handleVersionChange = (selectedOption) => {
-    setSelectedVersion(selectedOption.value);
+    try {
+      const apiResponse = await invoke('getDeliveries', { deliveryname: deliveryName });
+      console.log('API Response:', apiResponse);
+      setApiResponse(apiResponse);
+
+      if (apiResponse.length === 0) {
+        setError('Please enter a correct delivery name.');
+        return;
+      }
+
+      if (apiResponse.length > 1) {
+        setIsModalOpen(true);
+      } else {
+        const delivery = apiResponse[0];
+        await invoke('addDelivery', { version: selectedVersion, deliveryname: deliveryName, status: delivery.status });
+        setCurrentPage('list');
+      }
+    } catch (error) {
+      console.error('Error:', error);
+      setError('Failed to fetch delivery details. Please try again.');
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  const handleModalSubmit = async (selectedDelivery) => {
+    try {
+      if(currentPage === 'addDelivery')
+      {
+        await invoke('createVersionAndAddDelivery', { deliveryname: selectedDelivery.programName,projectId: projectId, status: selectedDelivery.status });
+        setIsModalOpen(false);
+          setCurrentPage('list');
+      }
+      await invoke('addDelivery', { version: selectedVersion, deliveryname: selectedDelivery.programName, status: selectedDelivery.status });
+      setIsModalOpen(false);
+      setCurrentPage('list');
+    } catch (error) {
+      console.error('Error:', error);
+      setError('Failed to add delivery. Please try again.');
+    }
+  };
+
+  const handleAddDeliverySubmit = async () => {
+    setLoading(true);
+    setError(null);
+      try {
+        const apiResponse = await invoke('getDeliveries', { deliveryname: deliveryName });
+        console.log('API Response:', apiResponse);
+        setApiResponse(apiResponse);
+  
+        if (apiResponse.length === 0) {
+          setError('Please enter a correct delivery name.');
+          return;
+        }
+  
+        if (apiResponse.length > 1) {
+          setIsModalOpen(true);
+        } else {
+          const delivery = apiResponse[0];
+          await invoke('createVersionAndAddDelivery', { deliveryname: deliveryName,projectId: projectId, status: delivery.status });
+          setCurrentPage('list');
+        }
+      } catch (error) {
+        console.error('Error:', error);
+        setError('Failed to fetch delivery details. Please try again.');
+      } finally {
+        setLoading(false);
+      }
   };
 
   
-  useEffect(() => {
-    if (currentPage === 'list') {
-      invoke('getRecords').then(setrecords);
-    }
-  }, [currentPage]); 
-
   const head = {
     cells: [
       {
-        key: "inumber",
-        content: "I-Number",
+        key: 'version',
+        content: 'Version',
         isSortable: true,
       },
       {
-        key: "name",
-        content: "Name",
+        key: 'deliveryname',
+        content: 'Delivery Name',
         isSortable: true,
       },
       {
-        key: "selectedVersion",
-        content: "Release Version",
+        key: 'status',
+        content: 'Status',
         isSortable: true,
       },
     ],
   };
 
-  const rows = records.map((record, index) => ({
-    key: `row-${index}-${record.inumber}`,
+  const rows = deliveries.map((delivery, index) => ({
+    key: `row-${index}-${delivery.deliveryname}`,
     cells: [
       {
-        key: `inumber-${record.inumber}`,
-        content: record.inumber,
+        key: `version-${delivery.version}`,
+        content: delivery.version,
       },
       {
-        key: `name-${record.name}`,
-        content: record.name,
+        key: `deliveryname-${delivery.deliveryname}`,
+        content: delivery.deliveryname,
       },
       {
-        key: `version-${record.selectedVersion}`,
-        content: record.selectedVersion,
+        key: `status-${delivery.status}`,
+        content: delivery.status,
       },
     ],
   }));
 
   let content;
-  if (currentPage === 'list') {
-    content = (
-      <>
-        <Text>All Entries</Text>
-        <DynamicTable
-          caption="List of Entries"
-          head={head}
-          rows={rows}
-        />
-        <Button onClick={goToEntry}>Add New Entry</Button>
-      </>
-    );
-  } else if (currentPage === 'entry') {
+  if (currentPage === 'entry') {
     content = (
       <>
         <Text>Entry Page</Text>
-        <Label labelFor='release'>Select Release Version: </Label>
+        {error && <Text>{error}</Text>}
+        <Label labelFor="version">Select Version:</Label>
         <Select
           appearance="default"
-          options={
-            projectVersions
-              ? projectVersions.values.map((version) => ({
-                  label: version.name,
-                  value: version.name,
-                }))
-              : []
-          }
-          onChange={handleVersionChange}
+          options={projectVersions.map((version) => ({
+            label: version.name,
+            value: version.name,
+          }))}
+          onChange={(selectedOption) => setSelectedVersion(selectedOption.value)}
           value={selectedVersion}
         />
-        <Label labelFor='name'>Enter Team-Member Name : </Label>
-        <Textfield id='name' onChange={(event) => { setname(event.target.value) }}></Textfield>
-        <Label labelFor='inumber'>Enter I-Number : </Label>
-        <Textfield id='inumber' onChange={(event) => setinumber(event.target.value)}></Textfield>
-
-        <Button onClick={goToList}>Add</Button>
+        <Label labelFor="deliveryname">Enter Delivery Name:</Label>
+        <Textfield
+          id="deliveryname"
+          value={deliveryName}
+          onChange={(event) => setDeliveryName(event.target.value)}
+        />
+        <Button onClick={handleSubmit} isDisabled={loading}>
+          {loading ? 'Loading...' : 'Submit'}
+        </Button>
+      </>
+    );
+  } else if (currentPage === 'list') {
+    content = (
+      <>
+        <Text>All Deliveries</Text>
+        <DynamicTable caption="List of Deliveries" head={head} rows={rows} />
+        <Button onClick={() => setCurrentPage('entry')}>Link New Delivery</Button>
+        <Button onClick={() => setCurrentPage('addDelivery')}>Create New Delivery</Button>
+      </>
+    );
+  } else if (currentPage === 'addDelivery') {
+    content = (
+      <>
+        <Text>Add Delivery</Text>
+        <Label labelFor="deliveryname">Enter Delivery Name:</Label>
+        <Textfield
+          id="deliveryname"
+          value={deliveryName}
+          onChange={(event) => setDeliveryName(event.target.value)}
+        />
+        <Button onClick={handleAddDeliverySubmit} isDisabled={loading}>
+          {loading ? 'Loading...' : 'Submit'}
+        </Button>
       </>
     );
   }
@@ -152,25 +232,32 @@ const App = () => {
   return (
     <>
       {content}
-      {data && <Text>Data from invoke: {JSON.stringify(data)}</Text>}
-      {context && (
-        <>
-          <Text>Context: {JSON.stringify(context)}</Text>
-          <Text>Project Key: {context.extension.project.key}</Text>
-          <Text>Project Id: {context.extension.project.id}</Text>
-        </>
-      )}
-
-      {/* Display project versions */}
-      {loading && <Text>Loading project versions...</Text>}
-      {error && <Text>Error: {error}</Text>}
-      {projectVersions && (
-        <>
-          <Text>Project Versions:</Text>
-          <Text>Context: {JSON.stringify(projectVersions.values[0].id)}</Text>
-          <Text>Context: {JSON.stringify(projectVersions.values[1].id)}</Text>
-        </>
-      )}
+      <ModalTransition>
+        {isModalOpen && (
+          <Modal onClose={() => setIsModalOpen(false)}>
+            <ModalHeader>
+              <ModalTitle>Select Delivery</ModalTitle>
+            </ModalHeader>
+            <ModalBody>
+              <Text>Multiple deliveries found. Please select one:</Text>
+              {apiResponse.map((delivery, index) => (
+                <Button
+                  key={index}
+                  appearance="primary"
+                  onClick={() => handleModalSubmit(delivery)}
+                >
+                  {delivery.programName}
+                </Button>
+              ))}
+            </ModalBody>
+            <ModalFooter>
+              <Button appearance="subtle" onClick={() => setIsModalOpen(false)}>
+                Cancel
+              </Button>
+            </ModalFooter>
+          </Modal>
+        )}
+      </ModalTransition>
     </>
   );
 };
--- src/resolvers/index.js
+++ src/resolvers/index.js
@@ -1,38 +1,26 @@
 import Resolver from '@forge/resolver';
-import api, { route } from '@forge/api';
 import { storage } from '@forge/api';
+import api, { route, fetch } from '@forge/api';
 
 const resolver = new Resolver();
 
 
-resolver.define('getText', (req) => {
-  console.log(req);
-  return 'Hello, world!';
-});
-
-
 resolver.define('getProjectVersions', async (req) => {
-  const { projectKey } = req.payload; 
+  const { projectKey } = req.payload;
 
   try {
-    
-    const response = await api.asUser().requestJira(route`/rest/api/3/project/${projectKey}/version`, {
+    const response = await api.asUser().requestJira(route`/rest/api/3/project/${projectKey}/versions`, {
       headers: {
-        'Accept': 'application/json'
-      }
+        'Accept': 'application/json',
+      },
     });
-
-    console.log(response);
-
-    console.log('Response status:', response.status); 
 
     if (!response.ok) {
       throw new Error(`Failed to fetch project versions: ${response.statusText}`);
     }
 
     const data = await response.json();
-    console.log('API response:', data); 
-    return data; 
+    return data;
   } catch (error) {
     console.error('Error fetching project versions:', error);
     throw new Error('Failed to fetch project versions');
@@ -40,22 +28,72 @@ resolver.define('getProjectVersions', async (req) => {
 });
 
 
-resolver.define('addrecords', async (req) => {
-  const { selectedVersion, name, inumber } = req.payload;
+resolver.define('getDeliveries', async (req) => {
+  const { deliveryname } = req.payload;
+  const url = `https://jira-dev.tools.sap/rest/siriusservices/1.0/siriusjiraintegration/deliveryforprogram?projectId=48389&callForCurrentProgram=false&createDelivery=true&programOrDeliveryName=${deliveryname}`;
 
-  const allrecords = await storage.get('records') || [];
-
-  const newrecord = { selectedVersion: selectedVersion, name: name, inumber: inumber };
-
-  allrecords.push(newrecord);
-
-  await storage.set('records', allrecords);
+  const response = await fetch(url, { method: 'GET' });
+  const data = await response.json();
+  return data;
 });
 
 
-resolver.define('getRecords', async () => {
-  const records = await storage.get('records') || [];
-  return records;
+resolver.define('addDelivery', async (req) => {
+  const { version, deliveryname, status } = req.payload;
+  const deliveries = await storage.get('deliveries') || [];
+  deliveries.push({ version, deliveryname, status });
+  await storage.set('deliveries', deliveries);
+});
+
+resolver.define('createVersionAndAddDelivery', async (req) => {
+  const { deliveryname, projectId, status} = req.payload;
+
+  
+  const versionName = `D_${deliveryname}`;
+  const description = `Linking Delivery ${deliveryname}`;
+
+  
+  const bodyData = JSON.stringify({
+    archived: false,
+    description: description,
+    name: versionName,
+    projectId: projectId,
+    releaseDate: new Date().toISOString().split('T')[0], 
+    released: true,
+  });
+
+  try {
+    
+    const response = await api.asUser().requestJira(route`/rest/api/3/version`, {
+      method: 'POST',
+      headers: {
+        'Accept': 'application/json',
+        'Content-Type': 'application/json',
+      },
+      body: bodyData,
+    });
+
+    if (!response.ok) {
+      throw new Error(`Failed to create version: ${response.statusText}`);
+    }
+
+    const versionData = await response.json();
+
+    
+    const deliveries = await storage.get('deliveries') || [];
+    deliveries.push({ version: versionName, deliveryname, status: status });
+    await storage.set('deliveries', deliveries);
+
+    return versionData;
+  } catch (error) {
+    console.error('Error creating version:', error);
+    throw new Error('Failed to create version');
+  }
+});
+
+resolver.define('getDeliveriesFromStorage', async () => {
+  const deliveries = await storage.get('deliveries') || [];
+  return deliveries;
 });
 
 export const handler = resolver.getDefinitions();
